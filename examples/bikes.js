4b556b63ea7fef73bc8a5e835c2f1a7e58e27250
Kevin Coughlin <kevintcoughlin@gmail.com>
2013-06-22 10:18:23 -0400

    formatting

--- examples/bikes.js
+++ examples/bikes.js
@@ -1,52 +1,5 @@
 /**
- *  Get a list of available stations and bikes.
- *     
-    Example Response:
-    {
-      ok: true,
-      meta: [ ],
-      results: [
-        {
-          id: 72,
-          status: "Active",
-          latitude: 40.76727216,
-          longitude: -73.99392888,
-          label: "W 52 St & 11 Av",
-          stationAddress: "",
-          availableBikes: 15,
-          availableDocks: 22,
-          nearbyStations: [
-            {
-            id: 480,
-            distance: 0.17780736685282
-            },
-            {
-            id: 508,
-            distance: 0.30285853404336
-            },
-            {
-            id: 495,
-            distance: 0.3196273377671
-            },
-            {
-            id: 422,
-            distance: 0.3446534955824
-            },
-            {
-            id: 449,
-            distance: 0.36516621792546
-            }
-          ]
-        },
-        
-        ... etc ...
-
-      ]
-      activeStations: 304,
-      totalStations: 327,
-      lastUpdate: 1370713417
-    }
- *  
+ *  Retrieves JSON data for stations, helmets, and bikes.
  */
  
 var Citibike = require('..')
