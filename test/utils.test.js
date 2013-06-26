7fb447de1b77fb5ec085940e138609b64a26c356
bdickason <dickason@gmail.com>
2013-06-25 23:49:45 -0400

    Moved utils test cases to use shouldJS to match other tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,28 +2,31 @@
    Test Framework: Mocha (https://github.com/visionmedia/mocha)
    Assertions: Should (https://github.com/visionmedia/should.js/) */
 
-var Citibike = require('..')
-  , utils = require('../lib/utils')
-  , assert = require('assert');
+var Citibike = require('..'),
+    utils = require('../lib/utils'),
+    should = require('should');
 
 describe('Citibike.utils', function () {
   describe('.merge()', function () {
-  	it('should return an object with the second object\'s values merged with the first.');
- 	var options = {
-		"key1": "value1",
-		"key2": "value2"
-	},
-	defaults = {
-		"key1": "default1",
-		"key2": "default2",
-		"key3": "default3"
-	},
-	result = {
-		"key1": "value1",
-		"key2": "value2",
-		"key3": "default3"
-	};
-	
-    assert.deepEqual(result, utils.merge(defaults, options));
+    it('should return an object with the second object\'s values merged with the first.', function() {
+
+    var options = {
+      "key1": "value1",
+      "key2": "value2"
+    },
+    defaults = {
+      "key1": "default1",
+      "key2": "default2",
+      "key3": "default3"
+    },
+    result = {
+      "key1": "value1",
+      "key2": "value2",
+      "key3": "default3"
+    };
+
+    result.should.eql(utils.merge(defaults, options));
+    
+    });
   });
 });
