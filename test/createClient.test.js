a772d10f9fc76ea11b20d79bb676c71a953e660d
bdickason <dickason@gmail.com>
2013-06-25 21:58:55 -0400

    Switched tests to should.js, fixed failing tests for createClient, corrected JSLint issues

--- test/createClient.test.js
+++ test/createClient.test.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var Citibike = require('..')
-  , utils = require('../lib/utils')
-  , assert = require('assert');
+var Citibike = require('..');
+var utils = require('../lib/utils');
+var should = require ('should');
 
 /** Tests for Citibike Client Creation */
 describe('Create citibike client', function () {
@@ -18,24 +18,24 @@ describe('Create citibike client', function () {
 				'User-Agent': 'node-citibike/'
 			},
 
-			rest_base: 				'http://appservices.citibikenyc.com/',
-			helmet_url: 			'v1/helmet/list',
-			branch_url: 			'v1/branch/list',
-			stations_url: 			'data2/stations.php',
-			stations_stream_url: 	'data2/stations.php?updateOnly=true',
+			rest_base: 'http://appservices.citibikenyc.com/',
+			helmet_url: 'v1/helmet/list',
+			branch_url: 'v1/branch/list',
+			stations_url: 'data2/stations.php',
+			stations_stream_url: 'data2/stations.php?updateOnly=true'
 		};
 
-        var citibike = new Citibike;
-      	assert.deepEqual(citibike.defaults, defaults);
+        var citibike = new Citibike();
+        citibike.defaults.should.eql(defaults);
     });
 
     it('should have correct API endpoints as defaults', function () {
-		var citibike = new Citibike;
-		assert.equal(citibike.defaults.rest_base, 'http://appservices.citibikenyc.com/');
-		assert.equal(citibike.defaults.helmet_url, 'v1/helmet/list');
-		assert.equal(citibike.defaults.branch_url, 'v1/branch/list');
-		assert.equal(citibike.defaults.stations_url, 'data2/stations.php');
-		assert.equal(citibike.defaults.stations_stream_url, 'data2/stations.php?updateOnly=true');
+		var citibike = new Citibike();
+		citibike.defaults.rest_base.should.equal('http://appservices.citibikenyc.com/');
+		citibike.defaults.helmet_url.should.equal('v1/helmet/list');
+		citibike.defaults.branch_url.should.equal('v1/branch/list');
+		citibike.defaults.stations_url.should.equal('data2/stations.php');
+		citibike.defaults.stations_stream_url.should.equal('data2/stations.php?updateOnly=true');
     });
 
     it('should merge options passed as parameter with defaults', function() {
@@ -49,19 +49,19 @@ describe('Create citibike client', function () {
 				'User-Agent': 'node-citibike/'
 			},
 
-			rest_base: 				'http://appservices.citibikenyc.com/',
-			helmet_url: 			'v1/helmet/list',
-			branch_url: 			'v1/branch/list',
-			stations_url: 			'data2/stations.php',
-			stations_stream_url: 	'data2/stations.php?updateOnly=true',
+			rest_base: 'http://appservices.citibikenyc.com/',
+			helmet_url: 'v1/helmet/list',
+			branch_url: 'v1/branch/list',
+			stations_url: 'data2/stations.php',
+			stations_stream_url: 'data2/stations.php?updateOnly=true',
 
 		},
-    	options = {
-    		api_key: 'users_key'
-    	};
+      options = {
+        api_key: 'users_key'
+      };
 
-    	var citibike = new Citibike(options);
-    	assert.deepEqual(citibike.options, utils.merge(defaults, options));
+      var citibike = new Citibike(options);
+      citibike.options.should.eql(utils.merge(defaults, options));
     });
   });
 });
