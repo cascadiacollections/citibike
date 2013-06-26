e601ba9759990768a69b89a5d59f543195ff72c1
bdickason <dickason@gmail.com>
2013-06-25 23:31:26 -0400

    Tests now pass for get() and added additional test case for invalid URL

--- test/citibike.test.js
+++ test/citibike.test.js
@@ -13,16 +13,25 @@ beforeEach(function(){
 describe('Citibike.get()', function() {
   it('should throw error if callback is not typeof function', function () {
     (function() {
-      citibike.get(null, null, 'string')
+      citibike.get(null, null, 'string');
     }).should.throwError('ERROR: Invalid callback function.');
+  });
 
-
-    /*
-    it('should successfully complete request', function (done) {
-      citibike.get(null, null, function(data) {
-        done();
+  it('should throw error if an invalid url is passed', function () {
+    (function() {
+      citibike.get(null, null, function(done) {
       });
-    }); */
+    }).should.throwError('ERROR: Invalid URL called.');
+  });
+
+  it('should successfully complete a request', function (done) {
+    var url=citibike.defaults.rest_base + citibike.defaults.helmet_url;
+
+    citibike.get(url, null, function(data) {
+      data.should.not.equal(null);
+      data.results.should.not.equal(null);
+      done();
+    });
   });
 });
 /*
