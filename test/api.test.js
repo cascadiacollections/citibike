077790b71db8962e8eacab353c6f290200d7466d
bdickason <dickason@gmail.com>
2013-06-29 11:46:23 -0400

    Added tests for Citibike Helmets API

--- test/api.test.js
+++ test/api.test.js
@@ -27,7 +27,7 @@ beforeEach(function(){
 describe('Citibike API - Branches', function () {
     it('responds with json', function (done) {
       request
-        .get('/v1/branch/list')
+        .get(citibike.defaults.branchesURL)
         .set('Accept', 'application/json')
         .expect(200)
         .end(function(err, res) {
@@ -37,10 +37,68 @@ describe('Citibike API - Branches', function () {
           res.header['content-type'].should.eql('application/json; charset=utf8');
 
           // Content
+          /* Expected Response:
+          {
+            ok: true,
+            meta: [ ],
+            results: [
+              {
+              id: 1,
+              latitude: 40.7086647301912,
+              longitude: -74.0108752995729,
+              label: "120 Broadway"
+              },
+            ],
+            lastUpdate: 1367853737
+          } */
+          
           res.body.ok.should.eql(true);
           res.body.results.should.not.be.empty;
 
           res.body.results[0].id.should.be.a('number');
+          res.body.results[0].latitude.should.be.a('number');
+          res.body.results[0].latitude.should.be.a('number');
+          res.body.results[0].label.should.be.a('string');
+
+          done();
+        });
+    });
+});
+
+describe('Citibike API - Helmets', function () {
+    it('responds with json', function (done) {
+      request
+        .get(citibike.defaults.helmetsURL)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end(function(err, res) {
+          should.not.exist(err);
+
+          // Headers
+          res.header['content-type'].should.eql('application/json; charset=utf8');
+
+          // Content
+          /* Expected Response:
+          {
+            ok: true,
+            meta: [ ],
+            results: [
+              {
+                id: 5002,
+                address: "571 Courtlandt Av",
+                latitude: 40.8170769363642,
+                longitude: -73.9193703979254,
+                label: "Neighborhood Cycle, Inc."
+              },
+            ],
+            lastUpdate: 1367853737
+          } */
+
+          res.body.ok.should.eql(true);
+          res.body.results.should.not.be.empty;
+
+          res.body.results[0].id.should.be.a('number');       
+          res.body.results[0].address.should.be.a('string');
           res.body.results[0].latitude.should.be.a('number');
           res.body.results[0].latitude.should.be.a('number');
           res.body.results[0].label.should.be.a('string');
