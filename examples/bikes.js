0179de4e85f43932b916b5f72ddc97d8959a9e03
Kevin Coughlin <kevintcoughlin@gmail.com>
2018-08-31 23:44:34 -0700

    + update examples

--- examples/bikes.js
+++ examples/bikes.js
@@ -1,24 +1,24 @@
 /**
  *  Retrieves JSON data for stations, helmets, and bikes.
  */
- 
-var Citibike = require('..')
-  , citibike = new Citibike;
+import Citibike from '..';
+
+const citibike = new Citibike();
 
 /** Retrieves Citibike Stations JSON Data and prints to console */
-citibike.getStations(null, function(data) {
-  console.log("Getting Citibike Stations...");
+citibike.getStations(null, (data) => {
+  console.log('Getting Citibike Stations...');
   console.log(data);
 });
 
 /** Retrieves Citibike Branches JSON Data and prints to console */
-citibike.getBranches(null, function(data) {
-  console.log("Getting Citibike Branches...");
+citibike.getBranches(null, (data) => {
+  console.log('Getting Citibike Branches...');
   console.log(data);
 });
 
 /**  Retrieves Citibike Helmets JSON Data and prints to console */
-citibike.getHelmets(null, function(data) {
-  console.log("Getting Citibike Helmets...");
+citibike.getHelmets(null, (data) => {
+  console.log('Getting Citibike Helmets...');
   console.log(data);
 });
