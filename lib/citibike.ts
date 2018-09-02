8946e1c1d23c62c36771fb5c9aa2a4965de607ca
Kevin Coughlin <kevintcoughlin@gmail.com>
2018-09-01 20:17:30 -0700

    simplify

--- lib/citibike.ts
+++ lib/citibike.ts
@@ -1,41 +1,28 @@
-import { defaults } from 'lodash';
 import fetch from 'node-fetch';
-import { isFunction } from 'util';
 import queryString from 'query-string';
 
-/**
- * API Client Options interface.
- */
-interface IOptions { apiKey: any; headers: { Accept: string; Connection: string; 'User-Agent': string; }; restBase: string; helmetsURL: string; branchesURL: string; }
+const BASE_URL = 'http://appservices.citibikenyc.com';
+const PATH_HELMETS = '/v1/helmet/list';
+const PATH_BRANCHE = '/v1/branch/list';
+const HEADERS = {
+  Accept: '*/*',
+  Connection: 'close',
+  'User-Agent': 'node-citibike/',
+};
 
 /**
  * Class for handling communications with Citibike's API.
  */
 export default class Citibike {
-  private _defaults: IOptions;
-  private _options: IOptions;
+  private _apiKey: string;
 
   /**
    * Constructor.
    * 
-   * @param {IOptions} options API options
+   * @param {string} the API key for Citibike's API.
    */
-  constructor(options?: IOptions) {
-    this._defaults = {
-      apiKey: null,
-  
-      headers: {
-        Accept: '*/*',
-        Connection: 'close',
-        'User-Agent': 'node-citibike/',
-      },
-  
-      restBase: 'http://appservices.citibikenyc.com',
-      helmetsURL: '/v1/helmet/list',
-      branchesURL: '/v1/branch/list',
-    };
-  
-    this._options = defaults(this._defaults, options);
+  constructor(apiKey?: string) {
+    this._apiKey = apiKey;
   }
 
   /**
@@ -45,13 +32,10 @@ export default class Citibike {
    * @param {Function}    callback    Callback function that will be called when the processing is done.
    * @param {Object}      params      Object containing query string parameters to issue in the Get request.
    */
-  private get(url: string, callback: Function, params?: object) {
-    if (!isFunction(callback)) throw new TypeError('callback is not a function');
-    if (url === null) throw new TypeError('url must not be null');
-  
+  private static get(url: string, callback: Function, params?: object) {
     const qs = queryString.stringify(params);
-    
-    fetch(`${this._options.restBase}${url}?${qs}`).then(res => callback(res.json()));
+
+    fetch(`${BASE_URL}${url}?${qs}`).then(res => callback(res.json()));
   }
 
   /**
@@ -60,8 +44,8 @@ export default class Citibike {
    * @param {Object}      params      Object containing query string parameters to issue in the request.
    * @param {Function}    callback    Callback function that will be called when the processing is done.
    */
-  public getBranches(params: object, callback: Function): void {
-    this.get(this._options.branchesURL, callback, params);
+  public static getBranches(params: object, callback: Function): void {
+    Citibike.get(PATH_BRANCHE, callback, params);
   }
 
   /**
@@ -70,7 +54,7 @@ export default class Citibike {
    * @param {Object}      params      Object containing query string parameters to issue in the request.
    * @param {Function}    callback    Callback function that will be called when the processing is done.
    */
-  public getHelmets(params: object, callback: Function): void {
-    this.get(this._options.helmetsURL, callback, params);
+  public static getHelmets(params: object, callback: Function): void {
+    Citibike.get(PATH_HELMETS, callback, params);
   }
 }
